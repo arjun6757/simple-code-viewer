7f629342b07936664d852aea7fbb740e62da1f31
arjun31 <arjunbanerjee13@gmail.com>
2024-12-02 18:51:50 +0530

    feat: add rate limiting to the API

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import api from './routes/api.js';
 import dotenv from 'dotenv';
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
@@ -10,8 +11,23 @@ const port = process.env.PORT || 3000;
 //middleware to parse json bodies
 app.use(express.json());
 
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after an hour",
+});
+
+app.use(limiter);
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');  // allow all origins for now
+  const allowedOrigin = process.env.FRONTEND_URL;
+  res.setHeader('Access-Control-Allow-Origin', allowedOrigin);  // allow all origins for now
+  res.setHeader('Access-Control-Allow-Methods', 'GET'); //only get req
+  
+   if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   next();
 })
 
