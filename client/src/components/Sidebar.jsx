05650f4f9135311588b6e648534c1599c3753644
arjun31 <arjunbanerjee13@gmail.com>
2024-12-13 13:06:58 +0530

    refactor: some minor changes

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -13,7 +13,7 @@ export default function Sidebar(props) {
       try {
         const selectedRepo = props.reposelect;
         const result = await fetch(
-          `http://localhost:3000/api/code/repo/select/${selectedRepo}`
+          `https://simple-code-viewer.onrender.com/api/code/repo/select/${selectedRepo}`
         );
         const data = await result.json();
         setRepoData(data);
@@ -33,7 +33,7 @@ export default function Sidebar(props) {
     const fetchRepoData = async () => {
       setLoading(true);
       try {
-        const result = await fetch("http://localhost:3000/api/code/repo");
+        const result = await fetch("https://simple-code-viewer.onrender.com/api/code/repo");
         const data = await result.json();
         setRepoData(data);
       } catch (error) {
