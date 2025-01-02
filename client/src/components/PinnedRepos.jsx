812f84ff6bfaf97cb68007ecab198aabf55043b4
Arjun Banerjee <133120290+arjun6757@users.noreply.github.com>
2025-01-02 19:39:27 +0530

    refactor: backend urls

--- client/src/components/PinnedRepos.jsx
+++ client/src/components/PinnedRepos.jsx
@@ -23,7 +23,7 @@ export default function PinnedRepos(props) {
       setLoading(true);
       try {
         const response = await fetch(
-          "http://localhost:3000/api/code/repo/usr/pinned",
+          "https://simple-code-viewer.onrender.com/api/code/repo/usr/pinned",
         );
 
         const responseData = await response.json();
